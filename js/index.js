ea86aaf22d14b2a7fb9f50de9d768953c0799ba5
Jennifer Lowe <jenniferlowe7@LIVE.com>
2020-05-31 16:51:36 +1200

    Remove opening functionality of the calendar

--- js/index.js
+++ js/index.js
@@ -20,15 +20,3 @@ function displayIndexDetails() {
   $(".detailsContainer").toggle();
   $(".related").toggleClass("fileBorder");
 }
-
-function w3_open() {
-  // document.getElementById("main").style.marginLeft = "25%";
-  document.getElementById("right-section").style.width = "15%";
-  document.getElementById("right-section").style.display = "inline-block";
-  document.getElementById("open-calendar").style.display = 'none';
-}
-function w3_close() {
-  //document.getElementById("main").style.marginLeft = "0%";
-  document.getElementById("right-section").style.display = "none";
-  document.getElementById("open-calendar").style.display = "inline-block";
-}
