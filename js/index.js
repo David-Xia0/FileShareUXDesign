cf61a546e1a36d9fef4991aa95f6005761133644
Jennifer Lowe <jenniferlowe7@LIVE.com>
2020-06-01 17:35:12 +1200

    Add highlighting to related files to calendar tasks

--- js/index.js
+++ js/index.js
@@ -22,18 +22,30 @@ function displayIndexDetails() {
   $(".relatedFile").toggle();
 }
 
+/**Click handler for the tasks on the calendar.*/
 $(".task").click(function() {
-  var hasClass = $(this).hasClass("task-clicked");
+  var id = "";
+  var taskIsHighlighted = $(this).hasClass("task-clicked");
 
   $(".task").removeClass("task-clicked");
-
-  if (!hasClass){
+  if (!taskIsHighlighted){
     $(this).toggleClass("task-clicked");
+    id = $(this).attr('id'); //id can now be used to highlight related files.
   }
 
-  var id = $(this).attr('id');
-
+  //Highlight appropriate related files in your drive.
+  $(".file").removeClass("calendar-related-file");
   if (id === "softeng-111"){
-    $("#index.html").toggleClass("calendar-related-file");
+    $("#index-html").toggleClass("calendar-related-file");
+
+  } else if (id === "get-doc-done"){
+    $("#style-css").toggleClass("calendar-related-file");
+    $("#index-js").toggleClass("calendar-related-file");
+
+  } else if (id === "softeng-250"){
+    $("#style-css").toggleClass("calendar-related-file");
+
+  } else if (id === "hand-in-brief"){
+    $("#assignment-brief").toggleClass("calendar-related-file");
   }
 });
