de2e58b050cb8a9131d7dc3bd0e0fe6d3a6a04f8
Jennifer Lowe <jenniferlowe7@LIVE.com>
2020-06-06 19:51:17 +1200

    Add some comments

--- js/index.js
+++ js/index.js
@@ -1,14 +1,15 @@
-// This function toggles the dispaly (hide/shows) property of the middle section
+/* This function toggles the display (hide/shows) property of the middle section */
 function hide(event) {
   var driveIsHighlighted = $(event).hasClass("selected");
+  $(".drive").removeClass("selected"); //Rid of all current selections.
 
-  $(".drive").removeClass("selected");
   // Highlight or remove highlighting of the selected drive
   if (driveIsHighlighted) {
     $(event).removeClass("selected");
   } else {
     $(event).toggleClass("selected");
   }
+
   // Toggle display of the middle section
   $(".middle-section").css("display", "none");
   switch (event.text) {
@@ -24,28 +25,32 @@ function hide(event) {
   }
 }
 
-/**Remove all highlighting and details pane popup, except for calendar highlighting.*/
+/* Remove all highlighting and details pane popup, except for calendar highlighting */
 function resetToDefaultMiddleSection() {
   $(".no-drive").css("display", "inline-block");
   $(".detailsContainer").hide();
   $(".file").removeClass("file-clicked");
   $(".relatedFile").hide();
 }
-// Toggle the display setting of the sucess pop up
+
+/* Toggle the display setting of the sucess pop up */
 function successPopup() {
   $(".pop-up-container").toggle();
   $(".success-container").toggle();
 }
-// Close the success pop up.
+
+/* Close the success pop up */
 function successClose() {
   successPopup();
   togglePopup();
 }
-// Toggle the share pop up.
+
+/* Toggle the share pop up */
 function togglePopup() {
   $(".pop-up").toggle();
 }
 
+/* Toggle function for the display panel. Only index.html pane was implemented */
 function displayDetails(event) {
   var id = $(event).attr("id");
 
@@ -53,12 +58,12 @@ function displayDetails(event) {
     $(".relatedFile").toggle();
     $(".detailsContainer").toggle();
   } else {
-    console.log("hi");
     $(".detailsContainer").hide();
     $(".relatedFile").hide();
   }
 }
 
+/* Second click handler for the files in the middle */
 $(".file").click(function() {
   var fileIsHighlighted = $(this).hasClass("file-clicked");
   $(".file").removeClass("file-clicked");
@@ -67,18 +72,19 @@ $(".file").click(function() {
   }
 });
 
-/**Click handler for the tasks on the calendar.*/
+/* Click handler for the tasks on the calendar */
 $(".task").click(function() {
   var id = "";
   var taskIsHighlighted = $(this).hasClass("task-clicked");
 
-  $(".task").removeClass("task-clicked");
+  $(".task").removeClass("task-clicked"); //Rid of all current selections.
+
   if (!taskIsHighlighted) {
     $(this).toggleClass("task-clicked");
     id = $(this).attr("id"); //id can now be used to highlight related files.
   }
 
-  //Highlight appropriate related files in your drive.
+  //Highlight appropriate related files in your drive, accordingly to the task clicked.
   $(".file").removeClass("calendar-related-file");
   if (id === "softeng-111") {
     $("#index-html").toggleClass("calendar-related-file");
